21923d3269418c3aa6f44f29c08dcc8350239366
Bryant Van Orden <138817470+BryantVanOrden@users.noreply.github.com>
2024-06-22 18:46:12 -0700

    hw

--- recipes/main.js
+++ recipes/main.js
@@ -3,25 +3,40 @@
 // Import the recipes from recipes.mjs
 import { recipes } from './recipes.mjs';
 
-window.onload = function() {
-    const recipesContainer = document.getElementById('recipes-container');
-    recipes.forEach(recipe => {
-        const recipeBox = document.createElement('div');
-        recipeBox.className = 'recipe-box';
-        recipeBox.innerHTML = `
-            <img src="${recipe.image}" alt="${recipe.name}" class="recipe-image">
-            <div class="recipe-details">
-                <span class="recipe-type">${recipe.tags.join(', ')}</span>
-                <h3>${recipe.name}</h3>
-                <div class="recipe-rating">${'★'.repeat(recipe.rating)}${'☆'.repeat(5 - recipe.rating)}</div>
-                <p>${recipe.description}</p>
-            </div>
-        `;
-        recipesContainer.appendChild(recipeBox);
-    });
+// Generate the recipe boxes dynamically
+const recipeBoxes = recipes.map(recipe => `
+    <div class="recipe-box">
+        <img src="${recipe.image}" alt="${recipe.name}" class="recipe-image">
+        <div class="recipe-details">
+            <span class="recipe-type">${recipe.tags.join(', ')}</span>
+            <h3>${recipe.name}</h3>
+            <div class="recipe-rating">${'★'.repeat(recipe.rating)}${'☆'.repeat(5 - recipe.rating)}</div>
+            <p>${recipe.description}</p>
+        </div>
+    </div>
+`).join('');
 
-    // Display debugging information
-    const debugInfo = document.createElement('pre');
-    debugInfo.textContent = JSON.stringify(recipes, null, 2); // Pretty-print the recipes array
-    document.body.appendChild(debugInfo);
-};
+// Create the main content
+const appContainer = document.getElementById('app');
+appContainer.innerHTML = `
+    <header>
+        <img src="images/recipe-book.png" alt="Recipe Book" id="header-image">
+        <img src="images/recipe-book-word.png" alt="Recipe Book" id="header-image">
+    </header>
+    <div id="search-container">
+        <input type="text" id="search-bar" placeholder="Search recipes...">
+        <button id="search-button"><img src="images/search.svg" alt="Search"></button>
+    </div>
+    <hr>
+    <main id="recipes-container">
+        ${recipeBoxes}
+    </main>
+    <footer>
+        <a href="https://www.flaticon.com/free-icons/recipe" title="Recipe icons">Recipe icons created by Freepik - Flaticon</a>
+        <div class="social-icons">
+            <img src="images/instagram_icon.svg" alt="Instagram">
+            <img src="images/pinterest_icon.svg" alt="Pinterest">
+            <img src="images/youtube_icon.svg" alt="YouTube">
+        </div>
+    </footer>
+`;
